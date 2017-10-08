975f55563922cfc8cbf0bc3f2ffb30dc19340bfb
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-10-09 00:24:01 +0530

    Added Odd Even program

---
+++ OddEven.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("Enter the number: \n");
+    scanf("%d\n", x);
+    if (x % 2 == 0)
+        printf("The entered number is even\n", );
+    else
+        printf("The entered number is odd\n", );
+}
